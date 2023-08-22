5e568a827d947ae6757b8119ef008dd4d04e51cb
gsong <rmsdlf1206@gmail.com>
2023-08-22 19:08:41 +0900

    cra clear

--- pocket_maddori_react/src/reportWebVitals.js
+++
@@ -1,9 +0,0 @@
-const reportWebVitals = (onPerfEntry) => {
-    if (onPerfEntry && onPerfEntry instanceof Function) {
-        import("web-vitals").then(
-            ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {}
-        );
-    }
-};
-
-export default reportWebVitals;
