c6f3f83b0c1115fdc077a9cdbeb80261ef51e231
Lim Taegyu <dlawork9888@gmail.com>
2023-08-22 19:12:30 +0900

    아 깃 어렵다

--- pocket_maddori_react/src/App.js
+++ pocket_maddori_react/src/App.js
@@ -1,6 +1,15 @@
+import React from 'react';
+
 function App() {
-    return;
-    <div></div>;
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <h1>Hello, React!</h1>
+            <h1>Hello, React!</h1>
+            <h1>Hello, React!</h1>
+        </div>
+    );
 }
 
 export default App;
+
