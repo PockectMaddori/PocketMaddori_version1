4f3c5b1670726d8fded9486f57f6b3fbce5ff2aa
gsong <rmsdlf1206@gmail.com>
2023-08-22 17:59:40 +0900

    first commit

---
+++ geun1/sex.js
@@ -0,0 +1,7 @@
+//문제: sex
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {}
